0d0550610ee6b0890bc380d8f385d926258e44b5
Meredith Tracy <meredithtracy@MacBook-Pro.local>
2022-02-25 17:37:55 -0800

    updated Database_Planning.sql doc

--- Database_Planning.sql
+++ Database_Planning.sql
@@ -38,22 +38,6 @@ CREATE TABLE avalanche_atlas (
 
 DROP TABLE avalanche_atlas
 
--- Join the avalanche_atlas and avalanche_obs tables
--- pause, may come back to this merge later
-SELECT 
-	avalanche_obs.avalanche_atlas_id,
-	avalanche_obs.obs_date_time, 
-	avalanche_obs."data.relative_size", 
-	avalanche_obs."data.path_elevation",
-	avalanche_atlas.id,
-	avalanche_atlas."data.aspect",
-	avalanche_atlas."data.start_elev",
-	avalanche_atlas."data.location_lat", 
-	avalanche_atlas."data.location_long"
-FROM avalanche_atlas
-LEFT JOIN avalanche_obs
-ON avalanche_obs.avalanche_atlas_id = avalanche_atlas.id;
-
 -- join the avalanche_obs and daily_obs tables to convert to csv
 -- for machine learning model
 SELECT 
@@ -74,5 +58,7 @@ SELECT
 INTO avalanche_data
 FROM daily_obs
 FULL OUTER JOIN avalanche_obs
-ON daily_obs.obs_date_time = avalanche_obs.obs_date_time;
+ON daily_obs.obs_date_time = avalanche_obs.obs_date_time
+WHERE daily_obs.obs_date_time >= '2019-01-01';
 
+DROP TABLE avalanche_data
